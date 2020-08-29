2956875e9825b549cd73e1163e15096d19dfb379
JoaoPGoes <joaopedro.goes13@gmail.com>
2020-08-29 20:35:39 -0300

    feat: create challenge features

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,11 @@
-import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const transactionType = type;
+
+    if (transactionType !== 'income' && transactionType !== 'outcome') {
+      throw Error('Type of transaction not avaliable');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    const transactionValues = this.transactionsRepository.getBalance();
+
+    if (transactionValues.total < value && transactionType === 'outcome') {
+      throw Error('Insufficient account value');
+    }
+
+    return transaction;
   }
 }
 
